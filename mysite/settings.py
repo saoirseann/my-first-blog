48c244ea39657acd3e53a01b021a449e9baa30eb
saoirseann <44141012+saoirseann@users.noreply.github.com>
2019-03-23 15:31:25 +0000

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&v81owsy+v@cnccib#cn9%)ttt6b1sa(5p3qer8$n0390f-%x4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
